9b2693fca2ff327fecd6366107f6f80a9427674b
FantaKiller <42299213+FantaKiller@users.noreply.github.com>
2019-11-01 22:05:17 +0800

    Add files via upload

---
+++ 1. C++与STL入门/map的两种赋值方式.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	map<string,int> faldo;
+	//map的两种赋值方式; 
+	faldo["fuck"]=3; 
+	faldo.insert(pair<string,int>("fuck",8));
+}
+//单引号只作用于单个字母常量;
+//双引号用于作用字符串;
